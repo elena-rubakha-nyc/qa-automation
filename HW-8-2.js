3147bd61aa221a949872513f00b7a4f9317c7d3f
Elena Rubakha <yelenarubakha@MacBook-Pro-3.local>
2023-07-16 23:16:17 -0400

    Homework 8.2

---
+++ HW-8-2.js
@@ -0,0 +1,43 @@
+let expencesExamples = [
+  {
+    yearlyExpences: [
+      2500, 6000, 7000, 6000, 3, 9, 700, 8000, 655, 5700, 340, 5390,
+    ],
+  },
+  {
+    yearlyExpences: [
+      500, 1000, 90000, 80, 980, 9000, 10, 400, 3000, 250, 45000, 1200,
+    ],
+  },
+  {
+    yearlyExpences: [
+      20, 200, 8900, 3300, 2300, 1009, 1200, 2000, 670, 900, 900, 7000,
+    ],
+  },
+];
+
+let Date = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+expencesExamples.forEach(function (value, index) {
+  let values = value.yearlyExpences;
+  console.log(`See months with expences <= $1000 for year ${index + 1}`);
+  values.forEach(function (value, index) {
+    let monthName = Date[index];
+    if (value <= 1000) {
+      console.log(monthName);
+    }
+  });
+});
